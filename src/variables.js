c77cbe97bd80f0453f00a4f07bfc960321cc2133
Lin <linmaweb@gmail.com>
2020-08-26 00:37:05 +1000

    initial commit

---
+++ src/variables.js
@@ -0,0 +1,27 @@
+const cardsContainer = document.getElementById("cards-container");
+const navigationContainer = document.getElementById("navigation");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const currentEl = document.getElementById("current");
+const showBtn = document.getElementById("show");
+const hideBtn = document.getElementById("hide");
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answer");
+const addCardBtn = document.getElementById("add-card");
+const clearBtn = document.getElementById("clear");
+const addContainer = document.getElementById("add-container");
+
+export {
+  cardsContainer,
+  navigationContainer,
+  prevBtn,
+  nextBtn,
+  currentEl,
+  showBtn,
+  hideBtn,
+  questionEl,
+  answerEl,
+  addCardBtn,
+  clearBtn,
+  addContainer,
+};
